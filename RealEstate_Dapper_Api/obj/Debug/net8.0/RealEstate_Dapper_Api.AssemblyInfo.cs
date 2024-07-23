8c51f259bd5ea7ff09ebdb4fd9a02b5bd8e6c910
AlihanCelik <alihan_08032003@hotmail.com>
2024-07-24 00:34:21 +0300

    a

--- RealEstate_Dapper_Api/obj/Debug/net8.0/RealEstate_Dapper_Api.AssemblyInfo.cs
+++ RealEstate_Dapper_Api/obj/Debug/net8.0/RealEstate_Dapper_Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate_Dapper_Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc28cbcdc46a5426058d6f14652a119045319507")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f400df026917ad8ed74ddd3e7b5527783cd4cd52")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate_Dapper_Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate_Dapper_Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
